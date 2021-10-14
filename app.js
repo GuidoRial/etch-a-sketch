262d04682957b1b3f88b0baa5ce5fc741f22ed22
GuidoRial <rialguido@gmail.com>
2021-10-14 19:41:16 -0300

    I made a clear botton and now there are as much divs as I need, but I cant make them stay in place

--- app.js
+++ app.js
@@ -8,35 +8,45 @@ let sizeSlider = document.querySelector(".size-slider");
 let grid = document.getElementById("#div-container");
 
 
+//change the value shown in the DOM as you slide the slider
 function updateSizeValue (value) {
     sizeValue.innerHTML = `${sizeSlider.value} x ${sizeSlider.value}`;
 }
 
+//when you change the value of the slider, change the size of the grid
 sizeSlider.addEventListener('input', function (e) {
     updateSizeValue();
     updateGrid();
 })
 
-
-
-function deleteDiv() {
-    let grid = document.querySelector("#div-container");
-    grid.removeChild(div);
-}
-
-//    let grid = document.querySelector("#div-container");
-//grid.removeChild(div);
-
-function updateGrid () {
-
+function createDivs () {
     for (let i = 0; i < sizeSlider.value * sizeSlider.value; i++) {
         let grid = document.querySelector("#div-container");
         const div = document.createElement("div");
         div.classList.add("div");
-        div.style.width = "512px";
-        div.style.height = "512px";
+
         div.style.border = "1px solid";
         grid.appendChild(div);
     }
 }
 
+
+
+
+
+function updateGrid () {
+    clearBoard();
+    createDivs();
+
+}
+
+function clearBoard () {
+    let clear = Array.from(document.querySelectorAll(".div"));
+    clear.forEach(wipe => wipe.remove());
+}
+
+//Now it only deletes the divs, in the future I want this botton to paint everything white, but by now this is good enough as i want to be able to fit the squares where they should go
+clearBtn.addEventListener("click", function (e) {
+    clearBoard();
+    console.log("jaja gil");
+})
