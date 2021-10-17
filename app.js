b9e25552ac507a4fe93a13d53aef7e640c634188
GuidoRial <rialguido@gmail.com>
2021-10-17 13:59:59 -0300

    project finished

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-let colorPicker = document.getElementById("#colorPicker");
-let colorBtn = document.getElementById("#colorBtn");
-let rainbowBtn = document.getElementById("#rainbowBtn");
-let eraserBtn = document.getElementById("#eraserBtn");
+let colorPicker = document.getElementById("colorPicker");
+let colorBtn = document.getElementById("colorBtn");
+let rainbowBtn = document.getElementById("rainbowBtn");
+let eraserBtn = document.getElementById("eraserBtn");
 let clearBtn = document.getElementById("clearBtn");
 let sizeValue = document.querySelector(".size-value");
 let sizeSlider = document.querySelector(".size-slider");
-let grid = document.getElementById("#div-container");
+let grid = document.getElementById("#container");
+let color = "black";
 
+
+createDivs();
 
 //change the value shown in the DOM as you slide the slider
 function updateSizeValue (value) {
@@ -21,10 +24,9 @@ sizeSlider.addEventListener('input', function (e) {
 
 function createDivs () {
     for (let i = 0; i < sizeSlider.value * sizeSlider.value; i++) {
-        let grid = document.querySelector("#div-container");
-        const div = document.createElement("div");
+        let grid = document.querySelector("#container");
+        let div = document.createElement("div");
         div.classList.add("div");
-        div.style.borderColor = "green";
         div.style.width = `${512 / sizeSlider.value}px`;
         div.style.height = `${512 / sizeSlider.value}px`;
         grid.appendChild(div);
@@ -32,20 +34,66 @@ function createDivs () {
 }
 
 
+
 function updateGrid () {
-    clearBoard();
+    deleteDivs();
     createDivs();
 
 }
 
-function clearBoard () {
+function deleteDivs () {
     let clear = Array.from(document.querySelectorAll(".div"));
     clear.forEach(wipe => wipe.remove());
 }
 
-//Now it only deletes the divs, in the future I want this botton to paint everything white, but by now this is good enough as i want to be able to fit the squares where they should go
+function clearBoard () {
+    deleteDivs();
+    createDivs();
+}
+//it deletes and adds the divs again, but they have the default background color (white)
 clearBtn.addEventListener("click", function (e) {
     clearBoard();
-});
+    activateColorMode();
+})
 
+function activateColorMode () {
+    console.log("color activated");
+    container.addEventListener('mouseover', function (e) {
+
+        e.target.style.backgroundColor = "black";
+
+    })
+}
+
+function activateRainbowMode () {
+    console.log("rainbow activated");
+    container.addEventListener('mouseover', function (e) {
+
+        e.target.style.backgroundColor = getRandomColor();
+
+})
+}
+
+function activateEraserMode () {
+    console.log("eraser activated");
+    container.addEventListener('mouseover', function (e) {
+
+        e.target.style.backgroundColor = "white";
+
+    })
+}
+
+colorBtn.addEventListener("click", activateColorMode);
+
+rainbowBtn.addEventListener("click", activateRainbowMode);
+
+eraserBtn.addEventListener("click", activateEraserMode);
+
+function getRandomColor() {
+    let red = String(Math.floor(Math.random() * 255));
+    let green = String(Math.floor(Math.random() * 255));
+    let blue = String(Math.floor(Math.random() * 255));
+  
+    return `rgb(${red}, ${green}, ${blue})`;
+  }
 
